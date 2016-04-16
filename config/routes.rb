0be3a80823da692ebff810dea78f9fbd4dafde3b
tallkeith <tallestkeith@gmail.com>
2016-04-16 11:03:05 -0400

    added destroy and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :comment
 
   post "sign_up", to: "sign_up#create"
+  delete "sign_up", to: "sign_up#destroy"
+  post "login", to: "sign_up#login"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
