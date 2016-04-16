27d3e291af042568cb790516aa73e38a66fe05ea
tallkeith <tallestkeith@gmail.com>
2016-04-16 12:24:09 -0400

    fixing paperclip issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
 	}
 
 	########### Begin Paperclip stuff ############
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	########### End Paperclip stuff ############
 
