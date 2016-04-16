1d7d64ccdf0f99c5a70fc9eb42f33e20bbfdb64d
tallkeith <tallestkeith@gmail.com>
2016-04-16 11:49:48 -0400

    fixed paperclip issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,11 @@ class User < ActiveRecord::Base
 		message: "MOOD IS EQUAL TO 1 THROUGH 2"
 	}
 
+	########### Begin Paperclip stuff ############
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+	########### End Paperclip stuff ############
+
 
 	def ensure_auth_token
 		unless self.auth_token
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416143458) do
+ActiveRecord::Schema.define(version: 20160416154853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,10 +43,14 @@ ActiveRecord::Schema.define(version: 20160416143458) do
     t.integer  "mood"
     t.text     "avatar"
     t.integer  "points"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "password_digest"
     t.string   "auth_token"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
