e44d701edb2d7f83537ccb96a61279e3623d0896
DaveKeith <d.keith78@yahoo.com>
2016-04-16 09:08:01 -0400

    created posts_controller class

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+  before_action :authenticate!, only: [:new, :create, :edit, :update, :delete]
+
+  def index
+    @posts = Post.all
+    render :index, locals: { posts: @posts }
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    new_post = current_user.posts.create(title: params["title"],
+                           content: params["content"]
+                           active: true)
+    redirect_to :root
+  end
+
+  def edit
+    @post = Post.find(params["id"])
+    render :edit, locals: {post: @post}
+  end
+
+  def update
+    right_now = DateTime.now
+    @post = Post.find(params["id"])
+    @post.update(title: params["title"],
+                content: params["content"]
+                active: params["active"])
+    @post.updated_at = right_now
+    redirect_to :root
+  end
+
+  def show
+    @post = Post.find(params["id"])
+    render :show
+  end
+
+  def delete
+    @post = Post.find(params["id"])
+    comments = Comment.where(post_id: @post.id)
+    comments.each do |comment|
+      comment.destroy
+    end
+    @post.destroy
+    redirect_to :root
+  end
+end
