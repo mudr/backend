d5550a53ab8ce6df52137d247eb535ce10939045
DaveKeith <d.keith78@yahoo.com>
2016-04-16 10:31:01 -0400

    added mood to create method to allow for login at signup

--- app/controllers/sign_up_controller.rb
+++ app/controllers/sign_up_controller.rb
@@ -5,6 +5,7 @@ class SignUpController < ApplicationController
     @user = User.new(username: params['username'],
                      avatar: params['avatar'],
                      email: params['email'],
+                     mood: params['mood'],
                      password: params['password'])
     @user.ensure_auth_token
     if @user.save
@@ -31,7 +32,7 @@ class SignUpController < ApplicationController
     @user = User.find_by(username: params["username"])
     if @user.authenticate(params["password"])
       @user.destroy
-        render plain: "USER DESTROYED", 
+        render plain: "USER DESTROYED",
         status: :accepted
     else
       render json: { error: "UNAUTHORIZED" },
